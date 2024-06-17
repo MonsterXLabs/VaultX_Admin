29c24d4a19804c4926753c88f9b7904cbe7a8a92
plvannemanja <plvannemanja@gmail.com>
2024-06-17 03:00:12 -0700

    choe: update contract address

--- src/utils/contract.ts
+++ src/utils/contract.ts
@@ -2,7 +2,7 @@ import { getContract } from "thirdweb";
 import { polygon } from "thirdweb/chains";
 import { client } from "./client";
 // get a contract
-const addr = import.meta.env.VITE_CONTRACT_ADDRESS || "0xE9bBE398b3Bf34791Dac5F53952a8acf6286DE26";
+const addr = import.meta.env.VITE_CONTRACT_ADDRESS || "0xd68627231763aed59A521138Bd1C3FD53ECeafd4";
 export const contract = getContract({
   // the client you have created via `createThirdwebClient()`
   client,
