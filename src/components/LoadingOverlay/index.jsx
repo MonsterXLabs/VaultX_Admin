337cd279e99db450838bfc289f5dee76c8d4403e
Nemanja Plvan <168236448+plvannemanja@users.noreply.github.com>
2024-07-13 05:37:42 +0800

    chore: update css in spin loading

--- src/components/LoadingOverlay/index.jsx
+++ src/components/LoadingOverlay/index.jsx
@@ -1,7 +1,6 @@
 // LoadingOverlay.js
 import React from 'react';
 import ClipLoader from 'react-spinners/ClipLoader';
-import './SpinnerOverlay.css'; // Import the CSS for overlay and spinner
 
 const LoadingOverlay = ({ loading }) => {
   if (!loading) return null;
