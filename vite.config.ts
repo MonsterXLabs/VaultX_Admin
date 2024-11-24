5f19b3b6e73746c87a7b6a020ac5b7dca124e29e
DESKTOP-6N5G11B\admin <edwardmintel@gmail.com>
2024-11-24 02:51:31 -0800

    feat: prod build

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,51 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path"
+import nodePolyfills from 'rollup-plugin-node-polyfills';
+import inject from '@rollup/plugin-inject';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      // Polyfill Node.js modules
+      crypto: 'crypto-browserify',
+      stream: 'stream-browserify',
+      buffer: 'buffer',
+      './runtimeConfig': './runtimeConfig.browser',
+    },
+  },
   optimizeDeps: {
     esbuildOptions: {
       define: {
         global: 'globalThis',
       },
     },
+    // disabled: false,
   },
   publicDir: 'public',
   build: {
-    outDir: 'build', // Custom output directory
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    sourcemap: true,
+    commonjsOptions: {
+      include: [/node_modules/],
+      extensions: ['.js', '.cjs'],
+      strictRequires: true,
+      // https://stackoverflow.com/questions/62770883/how-to-include-both-import-and-require-statements-in-the-bundle-using-rollup
+      transformMixedEsModules: true,
     },
+    rollupOptions: {
+      manualChunks: {
+        vendor: ['react', 'react-dom'], // Extract common libraries into a separate chunk
+      },
+      plugins: [
+        nodePolyfills(),
+        // inject({
+        //   Buffer: ['buffer', 'Buffer'], // Inject Buffer
+        // }),
+      ],
+    },
+    outDir: 'build', // Custom output directory
   },
 });
