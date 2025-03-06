6c20ae198971babb231f31e32111b215576b3ff8
DESKTOP-ASCN40O\Admin <topusmdev@gmail.com>
2025-03-05 22:17:45 -0800

    feat: change pinata

--- src/utils/pinata.ts
+++ src/utils/pinata.ts
@@ -1,27 +1,27 @@
-import { PinataSDK } from 'pinata';
+import { PinataSDK } from "pinata";
 
-export const PINATA_JWT =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiJiZTZkNDRmZC1mZDVmLTQ2MjMtODlkNy1kZjNmNDUzZjgxYTkiLCJlbWFpbCI6ImdvbGRlbi5kcmFnb24xMDI5MUBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwicGluX3BvbGljeSI6eyJyZWdpb25zIjpbeyJkZXNpcmVkUmVwbGljYXRpb25Db3VudCI6MSwiaWQiOiJGUkExIn0seyJkZXNpcmVkUmVwbGljYXRpb25Db3VudCI6MSwiaWQiOiJOWUMxIn1dLCJ2ZXJzaW9uIjoxfSwibWZhX2VuYWJsZWQiOmZhbHNlLCJzdGF0dXMiOiJBQ1RJVkUifSwiYXV0aGVudGljYXRpb25UeXBlIjoic2NvcGVkS2V5Iiwic2NvcGVkS2V5S2V5IjoiYmEyMWNlNTJjYTU2YWQwNjY0ZjkiLCJzY29wZWRLZXlTZWNyZXQiOiJkNTEyMmI4YjUzNjFkOGZjYzBhOGQ0NzI1MmQyYTIxYWI3ODNkYTczMjg2YmI5NDFkYmYwOWFiNTgxYTNjZmFiIiwiZXhwIjoxNzU2MzEzMDkxfQ.Jw60Rc0Z5NURw9zhgriqQc7s4q2RbFY35iOsrp_r5Kc';
-export const pinataGateway = 'tan-absent-fox-474.mypinata.cloud';
+const PINATA_JWT_SECRET = import.meta.env.VITE_PINATA_JWT_SECRET as string;
+const pinataGateway = import.meta.env.VITE_PINATA_GATEWAY as string;
+
 const pinata = new PinataSDK({
-  pinataJwt: PINATA_JWT!,
+  pinataJwt: PINATA_JWT_SECRET,
   pinataGateway: pinataGateway,
 });
 
 export const uploadMetaData = async (data: any) => {
   let response = await pinata.upload.json(data);
-  return pinataGateway + '/ipfs/' + response.IpfsHash;
+  return pinataGateway + "/ipfs/" + response.IpfsHash;
 };
 
 export const uploadFile = async (file: any) => {
-  console.log('upload file');
+  console.log("upload file");
   const newFile = new File([file], file.name, {
     type: file.type,
     lastModified: file.lastModified,
   });
   let response = await pinata.upload.file(newFile);
 
-  return pinataGateway + '/ipfs/' + response.IpfsHash;
+  return pinataGateway + "/ipfs/" + response.IpfsHash;
 };
 
 export const getData = async (uri: string) => {
